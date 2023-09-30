a94bf8a757c632b258f4c993ce5fe432c7e05932
Matt-JL-Burton <mslburton24@yahoo.com>
2023-09-30 18:50:51 +0100

    Write first attempt at a solution

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,7 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    dict = {}
+    for i in range(len(items)):
+        dict[str(items[i])] = dict.get(str(items[i]),0) + 1
     return frequencies
